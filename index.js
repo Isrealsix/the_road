4420930b65c71d0cc61dc1050fd03a548ef8683c
Isreal6ix <isrealsix@gmail.com>
2021-07-03 00:43:11 +0700

    added the navbar with stunning effects

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const cardButtons = document.querySelectorAll('.card__button');
+const container = document.querySelector('.container');
 
 // adding a click eventListener to all the buttons
 // such that the change class is toggled
@@ -6,3 +7,31 @@ cardButtons.forEach(button => {
 	button.onclick = ev =>
 		ev.target.parentElement.parentElement.classList.toggle('change');
 });
+
+// Colors
+const colors = ['#6495ed', '#7fffd4', '#ffa07a', '#f08080', '#afeeee'];
+const navbarLink = document.querySelectorAll('.navbar__link');
+// loop over the navbar link and add background color dynamically
+navbarLink.forEach((link, idx) => (link.style.backgroundColor = colors[idx]));
+
+const [open, close] = ['.navbar-icon--open', '.navbar-icon--close'];
+
+const [openIcon, closeIcon] = document.querySelectorAll(
+	'.navbar-icon--open, .navbar-icon--close'
+);
+
+const bringNavbar = icon => {
+	container.addEventListener('click', function (ev) {
+		const targetIcon = ev.target.closest(icon);
+		const notAvailable = ![openIcon, closeIcon].includes(targetIcon);
+		if (notAvailable) return;
+
+		if (targetIcon === openIcon) container.classList.add('change');
+
+		if (targetIcon === closeIcon) container.classList.remove('change');
+		// if (target === close) console.log('close');
+	});
+};
+
+bringNavbar(open);
+bringNavbar(close);
